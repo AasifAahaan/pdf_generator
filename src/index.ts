441900d23d398d7a91cbd8707abf29fa1c45e857
AasifAahaan <aasifalvi8888@gmail.com>
2024-09-19 22:37:31 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log('Hello,%20TypeScript%20with%20Node.js!');
+console.log('Hello,%20TypeScript%20with%20Node.js!');
+console.log('Hello,%20TypeScript%20with%20Node.js!');
+console.log('Hello,%20TypeScript%20with%20Node.js!');
